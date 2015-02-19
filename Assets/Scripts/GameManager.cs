b4dd35b49c00af14d7b1cfb775dc18957342aeaa
EverlessDrop41 <tomfperegrine@googlemail.com>
2015-02-19 21:47:38 +0000

    Initial Commit
    Contains really weird bug, throws error: NullReferenceException: Object
    reference not set to an instance of an object
    GameManager.Awake () (at
    Assets/Scripts/GameManager.cs:23)
    UnityEngine.Object:Instantiate(Object)
    Loader:Awake()
    (at Assets/Scripts/Loader.cs:11)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoardManager))]
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+	public BoardManager boardScript;
+	public int playerFoodPoints = 100;
+	[HideInInspector] public bool playersTurn = true;
+
+	private int level = 3;
+
+	void Awake() {
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy(this.gameObject);
+
+		DontDestroyOnLoad(this.gameObject);
+
+		boardScript = GetComponent<BoardManager>();
+		boardScript.SetupScene(level);
+		//InitGame();
+	}
+
+	void Start() {
+		Awake();
+	}
+
+	void InitGame(){
+		boardScript.SetupScene(level);
+	}
+
+	public void GameOver() {
+		enabled = false;
+	}
+}
